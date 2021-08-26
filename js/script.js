b6fe7d4f1842a54b0eb85b18a911cfc44ff72c25
zainHub19 <89556965+zainHub19@users.noreply.github.com>
2021-08-27 02:05:15 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav () {
+    let sidenav = document.getElementById("mySidenav")
+    sidenav.style.width = "250px"
+}
+function closeNav () {
+    let sidenav = document.getElementById("mySidenav")
+    sidenav.style.width = "0"
+}
