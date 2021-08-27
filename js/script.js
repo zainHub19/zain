e559488d059d53a9b6421c252402072dfb4f847f
zainHub19 <89556965+zainHub19@users.noreply.github.com>
2021-08-27 09:58:11 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function openNav () {
-    let sidenav = document.getElementById("mySidenav")
-    sidenav.style.width = "250px"
-}
-function closeNav () {
-    let sidenav = document.getElementById("mySidenav")
-    sidenav.style.width = "0"
-}
